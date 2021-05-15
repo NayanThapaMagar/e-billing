ece5b8a060f8ec4919eb28748b785069bee821f3
NayanThapaMagar <69561809+NayanThapaMagar@users.noreply.github.com>
2021-05-15 12:57:57 +0545

    Add files via upload

---
+++ frontend/stock/stock.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Stock</title>
+    <link rel="stylesheet" href="stock.css">
+</head>
+<body>
+    <h1>
+        STOCK
+    </h1>
+
+    <div >
+     <table id="table1" >
+         <tr>
+            <th>Name</th>
+            <th>ID</th>
+         </tr>
+         <tr>
+         <td class="input"><input type="text"></td>
+         <td class="input"><input type="text"></td>
+         </tr>
+     </table>
+    </div>
+
+    <div>
+        <button>Search</button>
+    </div>
+<br>
+<br>
+    <div >
+        <table id="table2">
+            <tr>
+                <th style="width: 40px;">SN</th>
+                <th style="width:150px">Particulars</th>
+             <th style="width: 130px;">Id</th>
+             <th style="width:120px;">Quantity</th>
+            </tr>
+
+            <tr style="height:25px;"> 
+                <td class="sn"> 1. </td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr style="height:25px;">
+                <td class="sn"> 2.</td>
+                <td ></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr style="height:25px;">
+                <td class="sn">3.</td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr style="height:25px;">
+                <td class="sn">4.</td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr style="height:25px;">
+                <td class="sn">5.</td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr style="height:25px;">
+                <td class="sn">6.</td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr> <tr style="height:25px;">
+                <td class="sn">7.</td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr> <tr style="height:25px;">
+                <td class="sn">8.</td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr> 
+            <tr style="height:25px;">
+                <td class="sn">9.</td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            
+        </table>
+    </div>
+
+
+</body>
+</html>
